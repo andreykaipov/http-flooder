aa8a0cee1726cff547fe9efba9113b7edbf0abea
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2020-05-04 19:22:01 -0400

    add http flooder

---
+++ flooder/aggregation.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"sync"
+	"time"
+)
+
+// Aggregation holds the aggregate info of how our server performed against our
+// requests.
+// Successes and Failures are reported as a counter.
+// TTFB and TTLB (time to [first|last] byte) are reported as averages.
+// The aggregation may be accessed by several goroutines concurrently.
+type Aggregation struct {
+	Successes int           `json:"successes"`
+	Failures  int           `json:"failures"`
+	TTFB      time.Duration `json:"ttfb"`
+	TTLB      time.Duration `json:"ttlb"`
+	mutex     sync.Mutex
+}
+
+// AddSuccess increments the aggregation's success counter, and adds the TTFB
+// and TTLB to their respective moving averages. We use the successes as the
+// divisor for our average.
+func (a *Aggregation) AddSuccess(ttfb, ttlb time.Duration) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+
+	a.Successes++
+	a.TTFB = a.TTFB + (ttfb-a.TTFB)/time.Duration(a.Successes)
+	a.TTLB = a.TTLB + (ttlb-a.TTLB)/time.Duration(a.Successes)
+}
+
+// AddFailure increments our aggregation's failure counter.
+func (a *Aggregation) AddFailure() {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+
+	a.Failures++
+}
+
+// PrettyPrint pretty prints our aggregation.
+func (a *Aggregation) PrettyPrint() {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+
+	total := a.Successes + a.Failures
+	fmt.Printf(`
+Total Requests: %d
+     Successes: %d
+      Failures: %d
+  Success Rate: %.5f%%
+  Failure Rate: %.5f%%
+  Average TTFB: %v
+  Average TTLB: %v
+         Delta: %v
+`,
+		total,
+		a.Successes,
+		a.Failures,
+		100*float64(a.Successes)/float64(total),
+		100*float64(a.Failures)/float64(total),
+		a.TTFB,
+		a.TTLB,
+		a.TTLB-a.TTFB,
+	)
+}
+
+// Write writes our aggregation to a file.
+func (a *Aggregation) Write(path string) error {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+
+	data, err := json.Marshal(a)
+	if err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(path, data, 0644); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ flooder/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptrace"
+	"os"
+	"sync"
+	"time"
+)
+
+var (
+	endpoint          string
+	requestsPerSecond int
+	duration          int
+	timeout           int
+	concurrency       int
+	maxRetry          int
+	report            string
+	verbose           bool
+)
+
+// TODO: support timeout and maxRetry flags
+//
+// Adding the timeout would involve modifying the HTTP client's transport to
+// just have a `Timeout` field (done). Then whenever a request times out or the
+// server "mysteriously" responds with an error, we can requeue this request.
+// Sounds good, but the tricky part is requeuing the request. We could make
+// a `requests` channel and use that to queue outgoing requests, at which point
+// requeuing would involve just putting it back onto the channel. Each request
+// should also know the current attempt it's on, as that's what we can use to
+// compare against our `maxRetry` flag.
+
+func init() {
+	flag.StringVar(&endpoint, "endpoint", "", "the endpoint to GET, e.g. http://cool-api:8080/wow")
+	flag.IntVar(&requestsPerSecond, "requests-per-second", 1, "number of GET requests per second to initiate against the endpoint")
+	flag.IntVar(&duration, "duration", 10, "how long would we like to run the test for (in seconds)?")
+	flag.IntVar(&timeout, "timeout", 1000, "timeout in milliseconds for the request to finish before retrying (unused)")
+	flag.IntVar(&maxRetry, "maxRetry", 3, "max retries to make for failed requests (unused)")
+	flag.StringVar(&report, "report", "", "output the report to a JSON file")
+	flag.BoolVar(&verbose, "verbose", false, "verbose logging while querying the servers")
+	flag.Parse()
+}
+
+func main() {
+	client := &http.Client{
+		Timeout: time.Duration(timeout) * time.Millisecond,
+		Transport: &http.Transport{
+			DisableKeepAlives: true,
+		},
+	}
+
+	agg := &Aggregation{}
+	jobs := sync.WaitGroup{}
+	secondsTicker := time.NewTicker(1 * time.Second)
+	tick := 0
+
+	fmt.Printf(`Starting flood. :-)
+Running for %d second(s), initiating %d request(s) per second. Total requests send to server will be %d.
+`,
+		duration,
+		requestsPerSecond,
+		duration*requestsPerSecond,
+	)
+flood:
+	for {
+		select {
+		case <-secondsTicker.C:
+			fmt.Println("Sending batch", tick)
+
+			tick++
+			if tick > duration {
+				secondsTicker.Stop()
+				break flood
+			}
+
+			go func() {
+				jobs.Add(1)
+				for i := 0; i < requestsPerSecond; i++ {
+					go func() {
+						jobs.Add(1)
+						go get(client, endpoint, agg)
+						jobs.Done()
+					}()
+				}
+				jobs.Done()
+			}()
+		}
+	}
+
+	jobs.Wait()
+	agg.PrettyPrint()
+}
+
+func get(client *http.Client, endpoint string, agg *Aggregation) {
+	var (
+		start     time.Time
+		firstByte time.Time
+		bodyRead  time.Time
+	)
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		agg.Failures++
+		fmt.Fprintf(os.Stderr, "Failed forming request %v: %v\n", endpoint, err)
+		return
+	}
+	req.Close = true
+
+	resp, err := client.Do(req.WithContext(httptrace.WithClientTrace(
+		req.Context(),
+		&httptrace.ClientTrace{
+			ConnectStart:         func(_, _ string) { start = time.Now() },
+			GotFirstResponseByte: func() { firstByte = time.Now() },
+		},
+	)))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed issuing request %v: %v\n", endpoint, err)
+		return
+	}
+
+	if resp.StatusCode/100 != 2 {
+		agg.Failures++
+		fmt.Fprintf(os.Stderr, "Received non-2xx response from server: %v\n", resp.StatusCode)
+		return
+	}
+
+	if _, err := io.Copy(ioutil.Discard, resp.Body); err != nil {
+		agg.Failures++
+		fmt.Fprintf(os.Stderr, "Failed reading response %v: %v\n", resp, err)
+		return
+	}
+
+	// Read the body. That was the last byte.
+	bodyRead = time.Now()
+
+	if err := resp.Body.Close(); err != nil {
+		agg.Failures++
+		fmt.Fprintf(os.Stderr, "Failed closing response body %v\n", err)
+		return
+	}
+
+	ttfb, ttlb := firstByte.Sub(start), bodyRead.Sub(start)
+
+	agg.AddSuccess(ttfb, ttlb)
+	if verbose {
+		fmt.Printf("ttfb=%s ttlb=%s delta=%s\n", ttfb, ttlb, ttlb-ttfb)
+	}
+}
